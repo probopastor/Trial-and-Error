55cb079a62f68de69fa86ad7e748500c32091f9a
jason-marks <44442842+jason-marks@users.noreply.github.com>
2020-02-06 16:03:41 -0600

    Added pseudocode for pushpull script

--- Trial & Error/Assets/Scripts/Luke Scripts/PushPull.cs
+++ Trial & Error/Assets/Scripts/Luke Scripts/PushPull.cs
@@ -13,9 +13,35 @@ public class PushPull : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*
+         * reticle creates raycast to determine if object can be interacted with
+         * if the reticle hits set a bool to true to indicate the player can interact
+         * 
+         * */
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
+            /*if the bool for walls is true, then 
+             *  if the raycast is above a certain range, then 
+             *      fly the player towards the raycasted obj
+             *      at the position of the raycast
+             *  if the raycast is below a certain range OR if the player is on a wall, then
+             *      push the player in the opposite direction of the raycast
+             * 
+             * 
+            */
+        }
 
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            /*if the bool for interactables is true, then 
+             *  if the game object for item in hand is null, then
+             *      set the game object for the raycasted item to in hand
+             *     if (Input.GetKeyUp(KeyCode.Mouse1))
+             *     {
+             *          throw it in the raycasted direction and set item in hand to null
+             *     }
+             * 
+             */
         }
     }
 }
